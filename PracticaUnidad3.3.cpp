a329b10b3e81cb00a7ece0c1b50a2ff90fe26aae
Fabrig96 <fabriag9@gmail.com>
2023-10-20 20:28:18 -0300

    Agregar archivos de proyecto.

---
+++ PracticaUnidad3.3.cpp
@@ -0,0 +1,37 @@
+// MAVI - Unidad 3 - Ejercicio3
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+Texture texture;
+Sprite sprite;
+
+int main() {
+
+
+	sf::RenderWindow App(sf::VideoMode(800, 600, 32), "Fondo");
+	
+	texture.loadFromFile("Unidad3_Assets/fondo.jpg");
+	sprite.setTexture(texture);
+	
+	float sizeTx = texture.getSize().x;
+	float sizeTy = texture.getSize().y;
+
+	sprite.setScale(800.0f/sizeTx, 600.0f/sizeTy);
+	while (App.isOpen())
+	{
+		sf::Event event;
+		while (App.pollEvent(event)) {
+			if (event.type == sf::Event::Closed)
+				App.close();
+		}
+
+		App.clear();
+
+		App.draw(sprite);
+
+		App.display();
+	}
+	return 0;
+}
